bf14196f09daadfaf46256e602efb9088656116e
Francesco La Camera <fm@lacamera.org>
2026-03-17 12:02:11 +0100

    migrate some configs to nix

--- modules/home/programs/ssh.nix
+++ modules/home/programs/ssh.nix
@@ -1,5 +1,15 @@
 { ... }:
 {
-  home.file.".ssh/config".source = ../../../config/home/ssh/config;
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      amd64 = {
+        hostname = "192.168.0.241";
+        user = "root";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+    };
+  };
+
   home.file.".ssh/allowed_signers".source = ../../../config/home/ssh/allowed_signers;
 }
